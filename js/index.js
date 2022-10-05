b92bc5ea30f7773774b3f74433bf75dee4e8051a
Hissue556 <hissue5.56@gmail.com>
2022-10-05 23:25:29 +0200

    .

--- js/index.js
+++ js/index.js
@@ -40,11 +40,9 @@ function addLoad() {
   transition_el.classList.add('is-active');
   setTimeout(() => {
     transition_el.classList.remove('is-active');
-  }, 499);
+  }, 500);
 };
 
 function delay (URL) {
   setTimeout( function() { window.location = URL }, 500 );
 };
-
-window.on
